72cb2abea216b78f8a5299b168080ceaa2e1802a
Young Bin Kim <lyakyb@gmail.com>
2015-04-08 20:20:47 -0600

    Initial Commit

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour
+{
+
+	public void LoadLevel (string name)
+	{
+		Debug.Log ("Hello I got here, WHATS THIS NAME? " + name);
+		Application.LoadLevel (name);
+	}
+
+	public void QuitRequest ()
+	{
+		Debug.Log ("Are you sure?");
+		Application.Quit ();
+	}
+
+}
---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour
+{
+	
+	int max;
+	int min;
+	int guess;
+
+	public int maxGuessAllowed = 5;
+	public Text text;
+
+	// Use this for initialization
+	void Start ()
+	{
+		StartGame ();
+	}
+
+	void StartGame ()
+	{
+		max = 1001;
+		min = 1;
+		NextGuess ();
+	}
+
+	public void GuessLower ()
+	{
+		max = guess;
+		NextGuess ();
+	}
+
+	public void GuessHigher ()
+	{
+		min = guess;
+		NextGuess ();
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		//if (guess <= min || max <= guess) {
+		//	Application.LoadLevel ("Cheat");}
+
+
+	}
+	
+	void NextGuess ()
+	{
+		guess = Random.Range (min, max + 1);
+
+		text.text = guess.ToString ();
+
+
+		maxGuessAllowed--;
+		if (maxGuessAllowed <= 0) {
+			Application.LoadLevel ("Win");
+		}
+	}
+
+}
